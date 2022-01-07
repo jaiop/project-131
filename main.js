d567c4a4fbb2e1bd9c6385ddcc684eb3d5d772f4
jaiop <86884090+jaiop@users.noreply.github.com>
2022-01-07 17:52:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bedroom()
+{
+  window.location = "bedroom.html";
+}
+function ac_and_tv()
+{
+  window.location = "ac and tv.html";
+}
+function desk()
+{
+  window.location = "desk.html";
+}
+function bottles()
+{
+  window.location = "bottles.html";
+}
+function fruit_basket()
+{
+  window.location = "fruit basket.html";
+}
